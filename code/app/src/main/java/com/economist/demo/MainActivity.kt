1d6c0d135c602766596adcef73ac119a15d4ff02
Devrath <166014800+devrath-teg@users.noreply.github.com>
2025-04-07 06:20:35 +0530

    centered the screen

--- code/app/src/main/java/com/economist/demo/MainActivity.kt
+++ code/app/src/main/java/com/economist/demo/MainActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -22,7 +25,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val viewModel: VideoPlayerViewModel = viewModel()
-            VideoList(viewModel = viewModel)
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                VideoList(viewModel = viewModel)
+            }
         }
     }
 }
